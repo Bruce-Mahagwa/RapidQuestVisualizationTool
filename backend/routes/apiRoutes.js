171022a57bc81b2fdcd044f9c6e50d208332cb82
Bruce-Mahagwa <jacobbruce880@gmail.com>
2024-08-20 13:10:45 +0300

    Adds sales controllers and customer controller files

---
+++ backend/routes/apiRoutes.js
@@ -0,0 +1,17 @@
+// variables
+const express = require("express");
+const app = express();
+// files
+// const categoryRoutes = require("./categoryRoutes");
+// const postRoutes = require("./postRoutes");
+// const spaceRoutes = require("./spaceRoutes");
+// const userRoutes = require("./userRoutes");
+
+
+
+// app.use("/categories", categoryRoutes);
+// app.use("/posts", postRoutes);
+// app.use("/spaces", spaceRoutes);
+// app.use("/users", userRoutes);
+
+module.exports = app;
